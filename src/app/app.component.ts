ad0298e85c45eda75e4c5e13b2fed9aaa62bfa11
Bacem BHA <bacbha@gmail.com>
2023-03-04 16:58:53 +0100

    final version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { app-teachers-list } from 'src/teachers/teachers-list/teachers-list.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,10 +8,5 @@ export class AppComponent {
   title = 'ScholarIT';
 }
 
-@Component({
-  selector: 'app-teachers-list',
-  templateUrl: './teachers-list.component.html',
-  styleUrls: ['./teachers-list.component.css']
-});
 
 
